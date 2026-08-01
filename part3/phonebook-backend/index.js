ca1cc1471de7255ac2b2f2adc72c5be5f3c6d619
Rakin <towfiqomarrakin@gmail.com>
2026-08-02 03:31:38 +0600

    3.16 Phonebook database step 3,4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,29 +3,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const Person = require('./models/person')
-
-let persons = [
-  { 
-    "id": "1",
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": "2",
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": "3",
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": "4",
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
+const { Result } = require('ethers')
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -57,17 +35,23 @@ app.get('/info', (request, response) => {
 
 })
 
-app.get('/api/persons/:id', (request, response)=>{
-  Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next)=>{
+  Person.findById(request.params.id)
+  .then(person => {
+    if(!person) {
+      response.status(404).end()
+    }
     response.json(person)
   })
+  .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id',(request, response)=>{
-  const id = request.params.id
-  persons = persons.filter(person=>person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+    .then(result =>{
+      response.status(204).end()
+    })  
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response)=>{
@@ -94,6 +78,26 @@ app.post('/api/persons', (request, response)=>{
     response.json(savedPerson)
   })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
