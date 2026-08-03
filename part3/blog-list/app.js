4a54c9d0b1996a885f7d10a745e74e89799396c5
towfiq-rakin <towfiqomarrakin@gmail.com>
2026-08-03 23:49:33 +0600

    4.2 Blog List, step 2

---
+++ part3/blog-list/app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('Connecting to ', config.MONGODB_URI)
+
+mongoose
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB', error.message)
+  })
+
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
